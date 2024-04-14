c6daccc9d2c8aab773bef3f46a915f4145d3ce97
kirtanchandak <themarketingkirtan@gmail.com>
2024-04-14 17:44:17 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
+  const images = [
+    "https://pbs.twimg.com/media/GHrURDQWoAA9-Ov?format=jpg&name=large",
+    "https://pbs.twimg.com/profile_images/1613212664788025350/blCkVHxp_400x400.jpg",
+    "https://media.licdn.com/dms/image/D4D22AQEvxSJJFitWcQ/feedshare-shrink_800/0/1697792194108?e=2147483647&v=beta&t=ctuD8IQ2C3jc6jVSsVCzMmPQa2_HXsM68iOipLvuP_0",
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>Jai shree ram!</h1>
+      </div>
+    </>
   );
 }
 
